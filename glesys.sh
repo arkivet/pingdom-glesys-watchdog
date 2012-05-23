0ed1805ecb019e3cdf353579b5908005cc4d6d66
David Eriksson <david@2good.nu>
2012-05-23 08:51:51 +0200

    Initial working version.
    If pingdom reports that the server is down, check server status at GleSYS.

---
+++ glesys.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+GLESYS_URL=https://api.glesys.com
+. `dirname $0`/config.sh
+
+METHOD="$1"
+shift
+RESOURCE="$1"
+shift
+
+glesys_get()
+{
+	_RESOURCE="$1"
+	curl --silent -u $GLESYS_USER:$GLESYS_PASSWORD $GLESYS_URL/$_RESOURCE/format/json | jsonpipe
+}
+
+case $METHOD in
+
+	GET)
+		glesys_get "$RESOURCE"
+		;;
+
+	*)
+		exit 1
+esac
+
+		
---
+++ pingdom.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+PINGDOM_URL=https://api.pingdom.com/api/2.0
+
+. `dirname $0`/config.sh
+
+METHOD=$1
+shift
+RESOURCE=$1
+shift
+
+pingdom_get()
+{
+	_RESOURCE=$1
+	curl --silent --header "App-Key: $PINGDOM_KEY" -u "$PINGDOM_USER:$PINGDOM_PASSWORD" $PINGDOM_URL/$_RESOURCE | jsonpipe
+}
+
+
+case $METHOD in 
+
+	GET)
+		pingdom_get "$RESOURCE"
+		;;
+
+	*)
+		exit 1
+		;;
+
+esac
+
